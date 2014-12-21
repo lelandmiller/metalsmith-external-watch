7a208f8621ea076e5049aadfbe45908f6c64a741
Leland Miller <lelandmiller@gmail.com>
2014-12-21 12:46:33 -0800

    Removed default yeoman test

--- test/test.js
+++ test/test.js
@@ -4,8 +4,10 @@ var assert = require('assert');
 var metalsmithExternalWatch = require('../');
 
 describe('metalsmith-external-watch node module', function () {
+    /*
     it('must have at least one test', function () {
         metalsmithExternalWatch();
         assert(false, 'I was too lazy to write any tests. Shame on me.');
     });
+    */
 });
